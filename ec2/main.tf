e1f15bff4682f5e7eb3da7d8631b754b53abbc49
chandradc <dcsekars_2000@yahoo.com>
2022-11-19 21:16:45 +0530

    u

--- ec2/main.tf
+++ ec2/main.tf
@@ -3,6 +3,17 @@ provider "aws"{
 region = "us-east-1"
 }
 
+terraform {
+  backend "s3" {
+    bucket  = "terraform-state-1040"
+    key     = "build/terraform.tfstate"
+    region  = "us-east-1"
+    profile = "dc-chandra"
+  }
+}
+
+
+"build/terraform.tfstate"
 resource "aws_vpc" "myvpc"{
 
 cidr_block = "10.0.0.0/16"
